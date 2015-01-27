6aea9477429cdc8b39e6860c75bcc3ff64df3bfd
Ubutus <ubutus@bk.ru>
2015-01-27 17:03:40 +0300

    Add a full_title helper

--- Gemfile
+++ Gemfile
@@ -6,12 +6,19 @@ gem 'rails', '4.0.2'
 
 group :development, :test do
   gem 'sqlite3', '1.3.8'
-  gem 'rspec-rails', '2.13.1'
+  gem 'rspec-rails', require: false
+gem 'guard'
+gem 'guard-bundler', require: false
+  gem 'guard-rspec', require: false
+  gem 'spork-rails', require: false
+  gem 'guard-spork', require: false
+  gem 'childprocess', '0.3.6'
 end
 
 group :test do
   gem 'selenium-webdriver', '2.35.1'
-  gem 'capybara', '2.1.0'
+  gem 'capybara', '2.2.0'
+  gem 'libnotify', '0.8.0'
 end
 
 gem 'sass-rails', '4.0.1'
--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -3,20 +3,25 @@ require 'spec_helper'
 describe "Static pages" do
 
   let(:base_title) { "Ruby on Rails Tutorial Sample App" }
-
+### HOME
   describe "Home page" do
 
     it "should have the content 'Sample App'" do
       visit '/static_pages/home'
       expect(page).to have_content('Sample App')
     end
-
-    it "should have the title 'Home'" do
+# изменения в связи с созданием хелпера full-title
+    it "should have the full-title" do
       visit '/static_pages/home'
-      expect(page).to have_title("#{base_title} | Home")
+      expect(page).to have_title("#{base_title}")
+    end
+
+ it "should not have a custom page title 'Home'" do
+      visit '/static_pages/home'
+      expect(page).not_to have_title('| Home')
     end
   end
-
+### HELP
   describe "Help page" do
 
     it "should have the content 'Help'" do
@@ -29,7 +34,7 @@ describe "Static pages" do
       expect(page).to have_title("#{base_title} | Help")
     end
   end
-
+### ABOUT
   describe "About page" do
 
     it "should have the content 'About Us'" do
@@ -42,7 +47,7 @@ describe "Static pages" do
       expect(page).to have_title("#{base_title} | About Us")
     end
   end
-
+### CONTACT
   describe "Contact page" do
 
     it "should have the content 'Contact'" do
